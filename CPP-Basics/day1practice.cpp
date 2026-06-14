085b3f19036e032038ee1e998ec7cdfd6b4f77df
sanjitpallar-tech <289798578+sanjitpallar-tech@users.noreply.github.com>
2026-06-14 18:48:26 +0530

    Create day1practice.cpp

---
+++ CPP-Basics/day1practice.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,sum;
+    cout<<"Enter the two numbers:"<<endl;
+    cin>>a>>b;
+    sum = a+b;
+
+    cout<<"Your sum is there:"<<sum<<endl;
+    return  0;
+}
