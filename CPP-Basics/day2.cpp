d06bca630b330357156a1fa1fab012c11a72c3a6
sanjitpallar-tech <289798578+sanjitpallar-tech@users.noreply.github.com>
2026-06-15 22:17:01 +0530

    DAY 2 DONE

---
+++ CPP-Basics/day2.cpp
@@ -0,0 +1,21 @@
+# include <iostream> 
+using namespace std;
+int main()
+{
+    int a , b , sum , difference , Product;
+    cout << " Enter the First number is :"<<endl;
+    cin>>a;
+    cout << "Enter the Second number is :"<<endl;
+    cin>>b;
+
+    sum = a + b;
+    difference = a - b;
+    Product = a * b;
+
+    cout << "enter the sum is : "<<sum<<endl;
+    cout << "enter the difference is : "<<difference<<endl;
+    cout << "enter the Product is : "<<Product<<endl;
+
+    return 0;
+
+}
---
+++ Python-Basics/day2.py
@@ -0,0 +1,10 @@
+side = float(input("Enter the square side is : "))
+
+print("area=", side*side)
+
+# Rectangle 
+
+side1 = float(input("Enter the Rectangle side1 is : "))
+side2 = float(input("Enter the Rectangle side2 is : "))
+
+print("area=", side1*side2)
