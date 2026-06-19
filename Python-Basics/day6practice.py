b4f08fcdaca798491dd2643099cbc6351517d82d
sanjitpallar-tech <289798578+sanjitpallar-tech@users.noreply.github.com>
2026-06-19 22:13:18 +0530

    DAY 6 Practice

---
+++ Python-Basics/day6practice.py
@@ -0,0 +1,25 @@
+# Multiple of '7'
+
+num = int(input("Enter the number is: "))
+
+if(num % 7 == 0):
+    print("It's Multiple of '7'")
+else:
+    print("It's not multiple of '7'")
+
+# Greatest number finder
+
+'''a = int(input("Enter the number is 'a': "))
+b = int(input("Enter the number is 'b': "))
+c = int(input("Enter the number is 'c': "))
+
+if(a > b and a > c):
+    print("'a' is greatest")
+
+elif(b > c and c > b):
+    print("'b' is greatest")
+
+elif(b > a and c > a):
+    print("'c' is greatest")'''
+
+
